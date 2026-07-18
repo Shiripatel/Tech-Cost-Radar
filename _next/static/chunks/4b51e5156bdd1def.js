89a2e930eb406de35b5f759ab29acf49a0114957
Shirish Patel <shirishpatel1611@gmail.com>
2026-07-17 21:40:29 -0400

    Deploy TechSpend AI with basePath configured

---
+++ _next/static/chunks/4b51e5156bdd1def.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/adcbe75ca5e9a1d7.js",
+  "static/chunks/turbopack-d1327901f3306450.js"
+])
---
+++ _next/static/chunks/aaff64d682441271.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/adcbe75ca5e9a1d7.js",
+  "static/chunks/turbopack-caa3f8b805edccc8.js"
+])
